3513fc9227a25a827dba4a5f235d487e4d890ee5
Lochie Ferrier <lochie@mit.edu>
2016-09-12 20:25:29 -0400

    halfway throhalfway through react tut

---
+++ learningreact/hw.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+ReactDOM.render();
+
+// tutorial1-raw.js
+var CommentBox = React.createClass({displayName: 'CommentBox',
+  render: function() {
+    return (
+      React.createElement('div', {className: "commentBox"},
+        "Hello, world! I am a CommentBox."
+      )
+    );
+  }
+});
+ReactDOM.render(
+  React.createElement(CommentBox, null),
+  document.getElementById('content')
+);
